677b213d16ee529ab37b9cf1c72afcee5739a4ac
Alex Schroeder <alex@gnu.org>
2015-09-15 14:59:33 +0200

    Actual spell checking happening

--- server.pl
+++ server.pl
@@ -1,10 +1,12 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use Mojo::ByteStream;
 use Text::Hunspell;
 use utf8;
 
-# Documentation browser under "/perldoc"
-plugin 'PODRenderer';
+# plugin 'TagHelpers';
+
+my @languages = qw(en_US);
 
 get '/' => sub {
     my $self = shift;
@@ -13,15 +15,52 @@ get '/' => sub {
 
 get '/check' => sub {
     my $self = shift;
+    $self->stash(languages => \@languages);
     $self->render('check');
 };
 
 post '/check' => sub {
     my $self = shift;
     my $text = $self->param('text');
-    # FIXME: do something interesting here
-    $self->render(template => 'result', result => $text);
+    my $lang = $self->param('lang') || 'en_US';
+    my $speller = Text::Hunspell->new(
+	"/usr/share/hunspell/$lang.aff",    # Hunspell affix file
+	"/usr/share/hunspell/$lang.dic"     # Hunspell dictionary file
+	);
+    die unless $speller;
+    
+    # What's a word? $1 is a word.
+    my $re;
+    $re = qr/(\w+(:?'\w+)*)/ if $lang =~ /^en/; # "isn't it"
+    $re = qr/(\w+)/ unless $re;
+
+    my @tokens;
+    my $last = 0;
+    while ($text =~ /\G(.*?)$re/gs) {
+	my ($stuff, $word) = ($1, $2);
+
+	# handle the stuff between words
+	if (length($stuff) > 0) {
+	    push(@tokens, $stuff);
+	}
+
+	if ($speller->check($word)) {
+	    push(@tokens, $word);
+	} else {
+	    push(@tokens, suggestions_for($word));
+	}
+
+	$last = pos;
+    }
+    $self->render(template => 'result', result => \@tokens);
 };
+
+sub suggestions_for {
+    my $word = shift;
+    # FIXME: do something
+    my $html = "<b>$word</b>";
+    return Mojo::ByteStream->new($html);
+}
 
 app->start;
 __DATA__
@@ -32,7 +71,7 @@ __DATA__
 <h1>Korero</h1>
 <blockquote>
 <b>kōrero</b>: <i>speech, narrative, story, news, account, discussion, conversation, discourse, statement, information</i><br>
-– <a href="http://www.maoridictionary.co.nz/search?keywords=korero">Te Aka Online Māori Dictionary</a>
+<span style="font-size:80%">– <a href="http://www.maoridictionary.co.nz/search?keywords=korero">Te Aka Online Māori Dictionary</a></span>
 </blockquote>
 <p>Welcome! Korero is a Swiss non-profit association supporting language with technical services.
 Our emphasis is on minority languages.
@@ -40,6 +79,7 @@ Our emphasis is on minority languages.
 <ul>
 <li><%= link_to 'Spellchecking' => 'check' %></li>
 </ul>
+
 
 @@ check.html.ep
 % layout 'default';
@@ -50,17 +90,23 @@ Back to the <%= link_to 'main page' => 'main' %>.
 <form method="POST" action="/check">
 <textarea name='text' autofocus='autofocus' required='required' maxlength='10000'></textarea>
 <p>
+<label for="lang">Languages:</label>
+%= select_field lang => [@$languages]
+<p>
 <input type="submit">
 </form>
+
 
 @@ result.html.ep
 % layout 'default';
 % title 'Korero Spellchecking';
 <h1>Korero Spellchecking</h1>
 <p>
-Back to the <%= link_to 'main page' => 'main' %>.
+Check a <%= link_to 'different text' => 'check' %> or go back to <%= link_to 'main page' => 'main' %>.
 <p class='result'>
-<%= $result %>
+% for my $token (@$result) {
+<%= $token %>\
+% }
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
@@ -72,8 +118,10 @@ body {
 }
 .result {
   border: 1px solid #333;
-  padding: 1ex;
+  padding: 0 1ex;
+  font-family: sans-serif;
   min-height: 20ex;
+  white-space: pre-wrap;
 }
 textarea {
   width: 100%;
