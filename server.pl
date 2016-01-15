f4e25bb1d161532fca1eafc35c7ef688fe872135
Alex Schroeder <alex@gnu.org>
2016-01-16 00:17:34 +0100

    Use --quiet for LAME

--- server.pl
+++ server.pl
@@ -172,7 +172,7 @@ post '/say' => sub {
       or die "Illegal voice: $voice";
   my ($out, $outname) = tempfile();
 
-  open(my $fh, "| espeak -v $voice --stdin --stdout | lame --preset voice - $outname")
+  open(my $fh, "| espeak -v $voice --stdin --stdout | lame --quiet --preset voice - $outname")
       or die "Cannot fork espeak/lame: $?";
   local $SIG{PIPE} = sub { die "Pipe to espeak/lame broke" };
 
